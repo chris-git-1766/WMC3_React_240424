92a45f925feb929c026142c6df0cbb73cf267f75
cabbageway <mail@kohlweg.com>
2023-12-19 10:43:35 +0100

    zweiter Commit

--- express_template/public/javascripts/main.ts
+++ express_template/public/javascripts/main.ts
@@ -31,16 +31,3 @@ children.push({
         }
     ]
 });
-children.push({
-    id: childrenID++,
-    name: "Felix",
-    age: 8,
-    wishes: [
-        {
-            id: wishID++,
-            name: "Tractor",
-            url: "https://www.kirstein.at/TL-Gitarren/Jet-Guitars-JT300-E-Gitarre-Blue.html?glp=1&msclkid=b04ffa2605ab174928b99f5f9fb989ac&utm_source=bing&utm_medium=cpc&utm_campaign=KirsteinAT_SH%20Smart%20Shopping%20A%20Produkte&utm_term=4575755098700001&utm_content=AG%20Smart%20ALL",
-            img_url: "https://www.kirstein.de/out/pictures/generated/product/1/1080_900_75/6d3d68608be35ca8801c3cabbbfdf74b_1.jpg"
-        }
-    ]
-});
